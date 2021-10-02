8d215a1b342c3001ad7b6dc1c73fd6a6443a7784
adwardstark <aditya.awasthi@arm.com>
2021-10-02 17:31:55 +0530

    Added toolbar, balance and card component

---
+++ app/components/ToolbarItem.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { View, TouchableOpacity, Image } from 'react-native'
+import {images} from '../resources/Images'
+
+export default function ToolbarItem({isBackVisible, onPress}) {
+    return (
+        <View style={styles.container}>
+            {
+                isBackVisible ? (
+                    <TouchableOpacity onPress={onPress}>
+                        <Image source={images.backArrow} 
+                            style={styles.backArrow}/>
+                    </TouchableOpacity>
+                ) : <View />
+            }
+            <Image source={images.aspireLogoGreen} 
+                style={{
+                    marginRight: 20
+                }}/>
+        </View>
+    )
+}
+
+const styles = {
+    container: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between"
+    },
+    backArrow: {
+        marginLeft: 20,
+        height: 16,
+        width: 14
+    }
+}
--- app/screens/DebitCard.js
+++ app/screens/DebitCard.js
@@ -1,13 +1,36 @@
 import React from 'react'
 import { View, Text, SafeAreaView } from 'react-native'
+import AvailableBalanceItem from '../components/AvailableBalanceItem'
+import CardItem from '../components/CardItem'
+import ToolbarItem from '../components/ToolbarItem'
 import { colors } from '../resources/Colors'
 
 export default function DebitCard() {
     return (
         <SafeAreaView style={{backgroundColor: colors.background, flexGrow: 1}}>
-            <View>
-                <Text>Debit Card</Text>
+            <View style={styles.container}>
+                <ToolbarItem />
+                <Text style={styles.header}>Debit Card</Text>
+                <AvailableBalanceItem amount="3,000"/>
+                <CardItem ownerName="Mark Henry" 
+                    expiry="Thru: 12/20" 
+                    cvv="456"
+                    cardNumber="5647341124132020"/>
             </View>
         </SafeAreaView>
     )
+}
+
+const styles = {
+    container: {
+        height: "100%",
+        width: "100%"
+    },
+    header: {
+        marginTop: 20,
+        marginLeft: 20,
+        fontSize: 24,
+        fontFamily: 'AvenirNextLTPro-Bold',
+        color: colors.white
+    }
 }
