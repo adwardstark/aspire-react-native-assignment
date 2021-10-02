0c8a113714d64517d69d889f857a5e2a182f52c4
adwardstark <aditya.awasthi@arm.com>
2021-10-02 14:04:16 +0530

    Added resources and navigation dependencies

--- App.js
+++ App.js
@@ -1,14 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import { useFonts } from 'expo-font';
+import { fonts } from './resources/Fonts';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  let [fontsLoaded] = useFonts(fonts);
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
---
+++ resources/Colors.js
@@ -0,0 +1,8 @@
+export const colors = {
+    white: "#FFFFFF",
+    black: "#222222",
+    background: "#0C365A",
+    accent: "#01D167",
+    blue: "#25345F",
+    grey: "#22222266"
+};
